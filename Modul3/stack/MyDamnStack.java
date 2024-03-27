c8840229d20076bec4d799c8272332f576d202e5
Muhammad Arif Irfan <marifirfannn@gmail.com>
2024-03-27 18:09:35 +0700

    update

---
+++ Modul3/stack/MyDamnStack.java
@@ -0,0 +1,64 @@
+package Tugas.stack;
+
+public class MyDamnStack {
+
+    private int Size;
+    private int top;
+
+    String[] MyURL;
+
+    public MyDamnStack(int AddSize) {
+        this.Size = AddSize;
+        MyURL = new String[Size];
+        top = -1;
+    }
+
+    public void visitURL(String URL) {
+        // if (top < Size-1 && top != Size-1) {
+        //     MyURL[++top] = URL;
+        // } 
+        
+        if (top == Size-1) {
+
+            String newArr[] = new String[2 * Size];
+
+            for (int i = 0; i < top+1; i++) {
+                newArr[i] = MyURL[i];
+            }
+            MyURL = newArr;
+            Size *= 2;
+        }
+
+        MyURL[++top] = URL;
+    }
+
+    public void back() {
+        if (top > 0) {
+            --top;
+        }
+    }
+
+    public void forward() {
+        if (top < Size - 1 && MyURL[top + 1] != null) {
+            ++top;
+        }
+    }
+
+    public void getCurrentURL() {
+        if (top != -1) {
+            System.out.println("Current URL : " + MyURL[top]);
+        }
+    }
+
+    public int getSize() {
+        return Size;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public int MyURL(){
+        return MyURL.length;
+    }
+}
